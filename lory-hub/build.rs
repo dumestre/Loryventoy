34f7e1103a9c1b0706386ca02c9c4287ce54efea
dumestre <djdumestre@outlook.com>
2026-07-26 00:12:58 -0300

    7.8

--- lory-hub/build.rs
+++ lory-hub/build.rs
@@ -1,3 +1,10 @@
 fn main() {
     slint_build::compile("ui/app.slint").unwrap();
+
+    #[cfg(target_os = "windows")]
+    {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("app.ico");
+        res.compile().unwrap();
+    }
 }
